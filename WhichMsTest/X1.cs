ac38c7a93a743d2ca3b8d3e333b589633ecf14c8
Adrian Sims <adriansims62781@gmail.com>
2020-08-11 14:24:38 +0200

    Add project files.

---
+++ WhichMsTest/X1.cs
@@ -0,0 +1,10 @@
+﻿namespace WhichMsTest
+{
+    public class X1
+    {
+        public int Method1()
+        {
+            return 1;
+        }
+    }
+}
---
+++ WhichMsTestTests/X1Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhichMsTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WhichMsTest.Tests
+{
+    [TestClass()]
+    public class X1Tests
+    {
+        [TestMethod()]
+        public void Method1Test()
+        {
+            var x1 = new X1();
+            Assert.ThrowsException<FileNotFoundException>(() => x1.Method1());
+            Assert.Fail();
+        }
+    }
+}
